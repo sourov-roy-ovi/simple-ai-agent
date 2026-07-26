e05246540198a18e89967dc7dd5b7f21433a3abd
Sourov Roy <info.sourovroy420@gmail.com>
2026-07-27 02:08:09 +0600

    Add AgentState definition to track conversation memory

--- agent.py
+++ agent.py
@@ -2,3 +2,11 @@ from typing import TypedDict, List, Union
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
+
+#========================================
+# 1. GRAPH STATE DEFINITION
+#========================================
+class AgentState(TypedDict):
+    """ Defines the schema fot the aget's shared memory state """
+    messages: List[Union[HumanMessage, AIMessage]]
+    
