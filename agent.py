70115b527eb26346c9a48aae908794ca6f7c187c
Sourov Roy <info.sourovroy420@gmail.com>
2026-07-27 02:25:13 +0600

    Initialize ChatOllama with Qwen2.5 14b model

--- agent.py
+++ agent.py
@@ -9,4 +9,12 @@ from langgraph.graph import StateGraph, START, END
 class AgentState(TypedDict):
     """ Defines the schema fot the aget's shared memory state """
     messages: List[Union[HumanMessage, AIMessage]]
-    
+
+#========================================
+# 2. LLM INITIALIZATION
+#========================================
+# Using Qwen 2.5 local instance via Ollma with deterministic sampling
+llm = ChatOllama(
+    model="qwen2.5:14b",
+    temperature=0
+)
